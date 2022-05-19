16991ccde869b3ceedcfd0896a5f8d3390ca33cb
Oussama <oussama.aouli@uit.uca.ma>
2022-05-19 21:35:47 +0200

    code corrector

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ from tkinter import *
 
 def convert_string(string):
     for e in string:
-        a = ord(e)
-        bnr = bin(a).replace('0b', '')
-        x = bnr[::-1]
-        while len(x) < 8:
-            x += '0'
-        bnr = x[::-1]
+        bnr = cvbn(ord(e), 8)
         print(bnr)
         r = ["X"] * 12
         ii = 0
@@ -22,7 +17,27 @@ def convert_string(string):
         print("".join(r))
 
 
-def set_cnrtl_bits(sqc):
+def set_control_bits(sqc):
+    print(sqc)
+    sqcrev = sqc[::-1]
+    mat = []
+    for j in range(len(sqcrev)):
+        if sqcrev[j] == "1":
+            print(list(cvbn(j+1, 4)))
+            mat.append(list(cvbn(j+1, 4)))
+    s = 0
+    print(mat)
+    z = []
+    for j in range(4):
+        for i in range(len(mat)):
+            s = s + int(mat[i][j])
+        z.append(s%2)
+        s = 0
+    print(z)
+
+
+
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -44,6 +59,15 @@ def create_matrix(string_length):
             matrix[i][j] = 1
 
 
+def cvbn(a, nbit):
+    # this will print a in binary
+    bnr = bin(a).replace('0b', '')
+    x = bnr[::-1]  # this reverses an array
+    while len(x) < nbit:
+        x += '0'
+    bnr = x[::-1]
+    return bnr
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
@@ -51,5 +75,7 @@ if __name__ == '__main__':
     canvas = Canvas(window, height=500, width=500)
     string = "leau"
     convert_string(string)
+    print(cvbn(12, 4))
+    set_control_bits("1101X000X1XX")
 
 # String length
